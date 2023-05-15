95795e618e9820371cca8bbdec8dbabca7e843f6
kubat <kubat.aiupov@gmail.com>
2023-05-15 19:51:40 +0200

    edit display password, add conditional rendering

--- src/components/GeneratePassword.jsx
+++ src/components/GeneratePassword.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
 const GeneratePassword = () => {
-
   const [lengthPassword, setLengthPassword] = useState(5);
-  const [showPassword, setShowPassword] = useState('---password will be here---')
+  const [showPassword, setShowPassword] = useState(
+    "---password will be here---"
+  );
 
   const passwordGenerator = (len) => {
     let password = "";
@@ -16,24 +17,34 @@ const GeneratePassword = () => {
     ];
     let charStr = chars.toString();
     for (let i = 0; i < len; i++) {
-      password += charStr.charAt(Math.floor(Math.random() * charStr.length))
+      password += charStr.charAt(Math.floor(Math.random() * charStr.length));
     }
-    return password
+    return password;
   };
 
   const handleClick = (e) => {
     e.preventDefault();
     setShowPassword(passwordGenerator(lengthPassword));
-    console.log(passwordGenerator(lengthPassword))
-  }
+    console.log(passwordGenerator(lengthPassword));
+  };
 
   //console.log(passwordGenerator(lengthPassword))
 
-  return <div>
-    <input type="text" value={lengthPassword} onChange={(e) => setLengthPassword(e.target.value)} />
-    <button onClick={handleClick}>Click</button>
-    <p>{showPassword}</p>
-  </div>;
+  return (
+    <div>
+      <input
+        type="text"
+        value={lengthPassword}
+        onChange={(e) => setLengthPassword(e.target.value)}
+      />
+      <button onClick={handleClick}>Click</button>
+      {!lengthPassword || lengthPassword == 0 ? (
+        <p>Please provide the length for the passoword</p>
+      ) : (
+        lengthPassword && <p>{showPassword}</p>
+      )}
+    </div>
+  );
 };
 
 export default GeneratePassword;
